28b8905cd46d097d96a6592f5c0bb467711a344c
Connor Baker <connorbaker@gmail.com>
2018-09-19 11:39:22 -0400

    simpleish kata

---
+++ primeFactors.rb
@@ -0,0 +1,28 @@
+class Factorizer
+  attr_accessor :factors
+  def initialize(n)
+    @factors = Hash.new(0)
+    factorize(n)
+  end
+
+  def factorize(n)
+    2.upto(n) do |e|
+      if n % e == 0
+        @factors[e] += 1
+        return factorize(n / e)
+      end
+    end
+  end
+
+  def print_string
+    str = ""
+    @factors.each_key do |key|
+      @factors[key] == 1 ? str += "(#{key})" : str += "(#{key}**#{@factors[key]})"
+    end
+    str
+  end
+end
+
+def primeFactors(n)
+  Factorizer.new(n).print_string
+end
