b3b577a3fd1ff2944288ad476f0fc3080580dac6
Connor Baker <connorbaker@gmail.com>
2018-09-20 15:58:28 -0400

    compare two arrays with nested structure

---
+++ nesting_structure_comparison.rb
@@ -0,0 +1,17 @@
+class Array
+  def same_structure_as(arr)
+    return false if arr.class != Array
+    self.each_index do |idx|
+      if self[idx].class != arr[idx].class
+        return false if self[idx].is_a?(Array) || arr[idx].is_a?(Array)
+      elsif self[idx].is_a?(Array)
+        if self[idx].length == arr[idx].length
+          return self[idx].same_structure_as(arr[idx])
+        else
+          return false
+        end
+      end
+    end
+    true
+  end
+end
